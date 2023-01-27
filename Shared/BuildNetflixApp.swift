91f2c3d48ddf3bcc98c008e188991c0ed2521537
Marcos Michel Machado Koch <marcos.machado@cwi.com.br>
2023-01-26 23:30:53 -0300

    Initial Commit

---
+++ Shared/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  Shared
+//
+//  Created by Marcos Michel on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
