8711c11469ef0dcd2973285f8763cb31c20e1143
Marcos Michel Machado Koch <marcos.machado@cwi.com.br>
2023-02-01 23:10:29 -0300

    add top movie preview

--- Shared/Home/Movie.swift
+++ Shared/Home/Movie.swift
@@ -11,4 +11,5 @@ struct Movie: Identifiable {
     var id: String
     var name: String
     var thumbnailURL: URL
+    var categories: [String]
 }
---
+++ Shared/Home/TopMoviePreview.swift
@@ -0,0 +1,61 @@
+//
+//  TopMoviePreview.swift
+//  BuildNetflix
+//
+//  Created by Marcos Michel on 01/02/23.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1 != catCount {
+                 return false
+            }
+        }
+        
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 3))
+                            }
+                        }
+                    }
+                }
+                
+                Text("Row of buttons")
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
